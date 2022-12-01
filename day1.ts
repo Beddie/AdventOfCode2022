8375ecca4647bfb3d2cff658acad215aaf0eec3a
Beddie <besselb@yahoo.com>
2022-12-01 20:50:18 +0100

    day1 start

--- day1.ts
+++ day1.ts
@@ -1,9 +1,25 @@
-import path from 'path';
-import fs from 'fs';
+import { readFileSync } from "fs";
+const puzzlePath = "puzzleinput/day1_1.txt"
+const puzzleString: string = readFileSync(puzzlePath).toString();
+const puzzleInput: string[] = puzzleString.split("/n");
 
-// from "./puzzleinput/day1_1.txt"
+class Elf {
+    calories: string[]
+}
 
-let message: string = "hello world"
-console.log(message);
+let elves: Elf[] = Elf[0];
 
-const puzzleinput = "./"
+//const Elves: string[] = puzzleInput.map((element) => element.);
+console.log(puzzleInput);
+let elf = new Elf;
+elves.push(elf);
+puzzleInput.forEach(line => {
+    if (line.length != 0){
+        elf.calories.push(line);
+    }
+    else {
+        elf = new Elf;
+        elves.push(elf);
+    }
+});
+
--- out/day1.js
+++ out/day1.js
@@ -1,2 +1,25 @@
-var message = "hello world";
-console.log(message);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+var puzzlePath = "puzzleinput/day1_1.txt";
+var puzzleString = (0, fs_1.readFileSync)(puzzlePath).toString();
+var puzzleInput = puzzleString.split("/n");
+var Elf = /** @class */ (function () {
+    function Elf() {
+    }
+    return Elf;
+}());
+var elves = Elf[0];
+//const Elves: string[] = puzzleInput.map((element) => element.);
+console.log(puzzleInput);
+var elf = new Elf;
+elves.push(elf);
+puzzleInput.forEach(function (line) {
+    if (line.length != 0) {
+        elf.calories.push(line);
+    }
+    else {
+        elf = new Elf;
+        elves.push(elf);
+    }
+});
