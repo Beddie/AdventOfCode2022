cb668842fc299c0dfc167da53b467227eb60bac5
Beddie <besselb@yahoo.com>
2022-12-05 22:12:00 +0100

    day5

--- day5.ts
+++ day5.ts
@@ -1,45 +1,67 @@
 import { readFileSync } from "fs";
 const puzzlePath = "puzzleinput/day5.txt";
-const puzzleString: string = readFileSync(puzzlePath).toString();
-const puzzleInputStacks: string[] = puzzleString.substring(0, puzzleString.indexOf('move')).split("\n");
-const puzzleInputProcedure: string[] = puzzleString.substring(puzzleString.indexOf('move')).split("\n");
 
-async function day5() {
-    let answer1 : number = 0;
-    let answer2 : number = 0;
+enum ProcedureIndex {
+     Move = 0
+    ,From = 1
+    ,To = 2
+}
+
+async function day5(part:number) {
+    const puzzleString: string = readFileSync(puzzlePath).toString();
+    const puzzleInputStacks: string[] = puzzleString.substring(0, puzzleString.indexOf('move')).split("\n");
+    const puzzleInputProcedure: string[] = puzzleString.substring(puzzleString.indexOf('move')).split("\n");
+
+    let answer : string = "";
 
     let line = "";
     while (line == ""){
         line = puzzleInputStacks.pop().trim()
     }
 
-    //let numbersOfStacks = line.split(/\s+/).length
-    const stackGrid : string[][] = []
-    puzzleInputStacks.reverse()
-    
+    //initialize grid
+    const stackGrid : string[][] = line.split(/\s+/).map((x) => [x])
+
+    //get first line
     let stackLine = puzzleInputStacks.pop();
-    let rowNumber = 0;
-    
-    while (stackLine && stackLine != ""){
-        stackLine += " "
-        let linestak : string[] = []
-        let lineStacks = Array.from(stackLine).reduce((total, x, i) => 
-        {
-            total += x
-            if ((i+1) % 4 == 0){
-                linestak[linestak.length] = total.trim().replace("[","").replace("]","")
-                total = ""
+
+    //fill stackGrid
+    while (stackLine && stackLine != "") {
+        stackLine += " ";
+        [...stackLine].reduce((crateLine, stackLineLetter, i) => {
+            i++
+            crateLine += stackLineLetter
+            if ((i) % 4 == 0){
+                let letter = crateLine.trim().replace("[","").replace("]","")
+                letter.length > 0 ? stackGrid[((i)/4) -1].push(letter): null;
+                crateLine = ""
             }
-        return total;
+            return crateLine;
         })
-        
-        stackGrid[rowNumber] = linestak
-        rowNumber++;
         stackLine = puzzleInputStacks.pop();
-
-        console.log(stackGrid.toString());
     }
+    
+    //create procedures
+    const procedureLines = puzzleInputProcedure
+                            .map((line)=> line.split(" "))
+                            .map<number[]>((words)=> [parseInt(words[1]), parseInt(words[3])-1, parseInt(words[5])-1])
 
+    //execute procedures
+    procedureLines.forEach((procedure)=> {
+        let moveCrates = []
+        for (let index = 0; index < procedure[ProcedureIndex.Move]; index++) {
+            moveCrates.push(stackGrid[procedure[ProcedureIndex.From]].pop());
+        }
+
+        let crates = part == 2 ? moveCrates.reverse() : moveCrates
+        stackGrid[procedure[ProcedureIndex.To]].push(...crates)
+    })
+
+    answer = stackGrid.map((crateStack) => { return crateStack.pop() }).join('')
+    return `answer${part} ${answer}`
 }
 
-day5();
+Promise.all([day5(1),day5(2)]).then((answer) => console.log(answer.join(', ')))
+
+//answer1 JCMHLVGMG
+//answer2 LVMRWSSPZ
