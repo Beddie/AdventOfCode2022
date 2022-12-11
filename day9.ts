3772e6704de91d5fcdf0cb2a72b32b07ad3279ac
Beddie <besselb@yahoo.com>
2022-12-11 11:35:59 +0100

    day9 wip

--- day9.ts
+++ day9.ts
@@ -13,7 +13,6 @@ enum Vector {
     rightOrUp = 1
 }
 
-
 class Motion {
     constructor(direction, val) {
         switch (direction) {
@@ -21,7 +20,7 @@ class Motion {
                 this.direction = Direction.up
                 break;
             case 'D':
-                this.direction = Direction.up
+                this.direction = Direction.down
                 break;
             case 'L':
                 this.direction = Direction.left
@@ -33,7 +32,6 @@ class Motion {
                 break;
 
         }
-         
         this.value = parseInt(val)
     }    
     public direction: Direction;
@@ -44,45 +42,86 @@ async function day9(part:number, print: boolean) {
     let answer = 0
     const puzzleInput = readFileSync(puzzlePath).toString().split('\n')
     const motions = puzzleInput.map(u => u.split(' ')).map((x,xindex) => new Motion(x[0],x[1]));
-    let Hx = 0;
-    let Hy = 0;
-    let Tx = 0;
-    let Ty = 0;
+    const headMovements = []
+    const tailMovements = []
+    let head = [0,0];
+    let tail = [0,0];
+    
     let vector: Vector
     motions.forEach((motion) => 
         {
-          
             const vector = motion.direction == Direction.up || motion.direction == Direction.right ? Vector.rightOrUp : Vector.leftOrDown;
-
+            
             for (let index = 0; vector == Vector.leftOrDown ? index > (motion.value * vector): index < (motion.value * vector); index+= vector) {
                 switch (motion.direction) {
                     case Direction.up:
                     case Direction.down:
-                        Hy += vector;
+                        head[1] += vector;
                         break;
                     case Direction.right:
                     case Direction.left:
-                        Hx += vector;
+                        head[0] += vector;
                         break;
                     default:
                         break;
                     }
-
-                console.log(`${motion.value} to ${Direction[motion.direction].toString()} \t:${Hx}-${Hy}/${index}`)
+                let newPosition = [head[0], head[1]];
+                headMovements.push(newPosition);
+                let distanceX = (head[0] < 0 ? head[0] * -1 : head[0]) - (tail[0] < 0 ? tail[0] * -1 : tail[0]);
+                distanceX = distanceX < 0 ? distanceX * -1 : distanceX
+                
+                let distanceY = (head[1] < 0 ? head[1] * -1 : head[1]) - (tail[1] < 0 ? tail[1] * -1 : tail[1]);
+                distanceY = distanceY < 0 ? distanceY * -1 : distanceY
+                let diagonal1place = distanceX == 1 && distanceY == 1
+                if (!diagonal1place && distanceX + distanceY > 1) {
+                    let tailx = headMovements[headMovements.length - 2][0]
+                    let taily = headMovements[headMovements.length - 2][1]
+                    tail[0] = tailx
+                    tail[1] = taily
+                    if (!tailMovements.find((tailxy) => tailxy[0] == tailx && tailxy[1] == taily)) {
+                        tailMovements.push([tailx,taily])
+                    }
+                } 
+                
+                drawGrid(head, tail, tailMovements)
+                
                 
             }
-        
             //y = motion.direction == Direction.up ?? 
-            
             //if head moves 2 positions from tail then tail moves to previous position from head
         }
     )
-
-    
     return answer
 }
 
 Promise.all([day9(1, true),day9(2, true)]).then((answer) => console.log(answer.join(', ')))
 
+
+function drawGrid(head: number[], tail: number[], tailMovements: any[]) {
+    let size = 8
+    let grid = ""
+    for (let y = size; y > -size; y--) {
+        
+        for (let x = -size; x < size; x++) {
+            let tailSpot = tailMovements.find(tailxy=> tailxy[0] == x && tailxy[1] == y)
+            if (x == head[0] && y == head[1]){
+                grid += tailSpot ? "h" : "H"
+            }
+            else if (x == tail[0] && y == tail[1]){
+                grid += tailSpot ? "t" : "T"
+            }
+            else if (tailSpot) {
+                grid += "*"
+            }
+            else {
+                grid += "."
+            }
+        }
+        grid += "\n"
+    }
+    console.log(grid);
+    let breakje = head[0] == 1 && head[1] == 4    
+    //console.log(`${motion.value} to ${Direction[motion.direction].toString()} \t:${head[0]}-${head[1]}/${index}`)
+}
 //answer1 
 //answer2 
--- out/day9.js
+++ out/day9.js
@@ -21,7 +21,7 @@ class Motion {
                 this.direction = Direction.up;
                 break;
             case 'D':
-                this.direction = Direction.up;
+                this.direction = Direction.down;
                 break;
             case 'L':
                 this.direction = Direction.left;
@@ -41,10 +41,10 @@ async function day9(part, print) {
     let answer = 0;
     const puzzleInput = (0, fs_1.readFileSync)(puzzlePath).toString().split('\n');
     const motions = puzzleInput.map(u => u.split(' ')).map((x, xindex) => new Motion(x[0], x[1]));
-    let Hx = 0;
-    let Hy = 0;
-    let Tx = 0;
-    let Ty = 0;
+    const headMovements = [];
+    const tailMovements = [];
+    let head = [0, 0];
+    let tail = [0, 0];
     let vector;
     motions.forEach((motion) => {
         const vector = motion.direction == Direction.up || motion.direction == Direction.right ? Vector.rightOrUp : Vector.leftOrDown;
@@ -52,16 +52,32 @@ async function day9(part, print) {
             switch (motion.direction) {
                 case Direction.up:
                 case Direction.down:
-                    Hy += vector;
+                    head[1] += vector;
                     break;
                 case Direction.right:
                 case Direction.left:
-                    Hx += vector;
+                    head[0] += vector;
                     break;
                 default:
                     break;
             }
-            console.log(`${motion.value} to ${Direction[motion.direction].toString()} \t:${Hx}-${Hy}/${index}`);
+            let newPosition = [head[0], head[1]];
+            headMovements.push(newPosition);
+            let distanceX = (head[0] < 0 ? head[0] * -1 : head[0]) - (tail[0] < 0 ? tail[0] * -1 : tail[0]);
+            distanceX = distanceX < 0 ? distanceX * -1 : distanceX;
+            let distanceY = (head[1] < 0 ? head[1] * -1 : head[1]) - (tail[1] < 0 ? tail[1] * -1 : tail[1]);
+            distanceY = distanceY < 0 ? distanceY * -1 : distanceY;
+            let diagonal1place = distanceX == 1 && distanceY == 1;
+            if (!diagonal1place && distanceX + distanceY > 1) {
+                let tailx = headMovements[headMovements.length - 2][0];
+                let taily = headMovements[headMovements.length - 2][1];
+                tail[0] = tailx;
+                tail[1] = taily;
+                if (!tailMovements.find((tailxy) => tailxy[0] == tailx && tailxy[1] == taily)) {
+                    tailMovements.push([tailx, taily]);
+                }
+            }
+            drawGrid(head, tail, tailMovements);
         }
         //y = motion.direction == Direction.up ?? 
         //if head moves 2 positions from tail then tail moves to previous position from head
@@ -69,6 +85,31 @@ async function day9(part, print) {
     return answer;
 }
 Promise.all([day9(1, true), day9(2, true)]).then((answer) => console.log(answer.join(', ')));
+function drawGrid(head, tail, tailMovements) {
+    let size = 8;
+    let grid = "";
+    for (let y = size; y > -size; y--) {
+        for (let x = -size; x < size; x++) {
+            let tailSpot = tailMovements.find(tailxy => tailxy[0] == x && tailxy[1] == y);
+            if (x == head[0] && y == head[1]) {
+                grid += tailSpot ? "h" : "H";
+            }
+            else if (x == tail[0] && y == tail[1]) {
+                grid += tailSpot ? "t" : "T";
+            }
+            else if (tailSpot) {
+                grid += "*";
+            }
+            else {
+                grid += ".";
+            }
+        }
+        grid += "\n";
+    }
+    console.log(grid);
+    let breakje = head[0] == 1 && head[1] == 4;
+    //console.log(`${motion.value} to ${Direction[motion.direction].toString()} \t:${head[0]}-${head[1]}/${index}`)
+}
 //answer1 
 //answer2 
 //# sourceMappingURL=day9.js.map
