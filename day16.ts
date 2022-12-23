38027e5121b9530d05054d8d7e0cf473bca8e767
Beddie <besselb@yahoo.com>
2022-12-23 21:37:52 +0100

    day16 part2 wip

--- day16.ts
+++ day16.ts
@@ -52,37 +52,39 @@ async function day16(part: number, print: boolean) {
     let tunnelstovalve = line.substring(line.indexOf('valves') + 6).split(',').map(valve => valve.trim())
     return new Valve(name, flowrate, tunnelstovalve)
   })
-
+  let totalValves = scanOutput.filter(so => so.FlowRate != undefined && so.FlowRate > 0).length
   let minute = 0;
   let newPaths: Step[] = [new Step("AA", 0)]
 
-  while (minute <= 30) {
+  if (part == 1) {
 
-    if (minute == 24) {
-      newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 2);
+    while (minute <= 30) {
 
-      newPaths.sort((a,b) => b.TotalPressure(30) - a.TotalPressure(30))
-      newPaths.splice(1000);
-    }
+      if (minute == 24) {
+        newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 2);
 
-    let oldPaths = newPaths.filter(np => np.Minute == minute)
-    minute++;
+        newPaths.sort((a, b) => b.TotalPressure(30) - a.TotalPressure(30))
+        newPaths.splice(1000);
+      }
 
-    console.log(minute);
-    //logPath(oldPaths, minute);
-    newPaths = [];
-    oldPaths.forEach(oldPath => {
-      let openValves = oldPath.OpenValves ? oldPath.OpenValves : []; //oldPath.OpenValves;
-      let currentValve = scanOutput.find(v => v.Name == oldPath.ValveName);
-      let valves = scanOutput.filter(v => currentValve.TunnelToValve.includes(v.Name));
-      let currentFlow = oldPath.TotalFlowRate ?? 0;
-      valves.filter(v => v.Name != oldPath.PreviousValve).forEach(valve => {
-        let openValvesMinute = oldPath.OpenValvesMinute ? oldPath.OpenValvesMinute : null;
+      let oldPaths = newPaths.filter(np => np.Minute == minute)
+      minute++;
+
+      console.log(minute);
+      //logPath(oldPaths, minute);
+      newPaths = [];
+      oldPaths.forEach(oldPath => {
+        let openValves = oldPath.OpenValves ? oldPath.OpenValves : []; //oldPath.OpenValves;
+        let currentValve = scanOutput.find(v => v.Name == oldPath.ValveName);
+        let valves = scanOutput.filter(v => currentValve.TunnelToValve.includes(v.Name));
+        let currentFlow = oldPath.TotalFlowRate ?? 0;
+        valves.filter(v => v.Name != oldPath.PreviousValve).forEach(valve => {
+          let openValvesMinute = oldPath.OpenValvesMinute ? oldPath.OpenValvesMinute : null;
           newPaths.push(new Step(valve.Name, minute, currentFlow, openValves, currentValve.Name, openValvesMinute))
-      })
+        })
 
-      
-      if (currentValve.FlowRate > 0 && (!openValves || !openValves.find(o => o === currentValve.Name))) {
+
+        if (currentValve.FlowRate > 0 && (!openValves || !openValves.find(o => o === currentValve.Name))) {
           let openValvesMinute = oldPath.OpenValvesMinute ? [...oldPath.OpenValvesMinute] : null; //oldPath.OpenValvesMinute;
           let newOpenValves = [...openValves]
           // if (openValvesMinute?.length > 6) {
@@ -103,16 +105,99 @@ async function day16(part: number, print: boolean) {
           let newFlow = currentFlow + currentValve.FlowRate
           newPaths.push(new Step(currentValve.Name, minute, newFlow, newOpenValves, currentValve.Name, openValvesMinute))
         }
-      
-    })
+
+      })
+    }
+
+    //let steps = newPaths.filter(np => np.Minute == minute)
+    let highPath = newPaths.sort((o1, o2) => o2.TotalPressure(30) - o1.TotalPressure(30))[0];
+
+    // let hp = newPaths.filter(a => a.TotalFlowRate == highPath.TotalFlowRate)[0];
+    let totalPressure = highPath.TotalPressure(30);
+    return totalPressure;
   }
+  else {
+    //part 2
 
-  //let steps = newPaths.filter(np => np.Minute == minute)
-  let highPath = newPaths.sort((o1, o2) => o2.TotalPressure(30) - o1.TotalPressure(30))[0];
+    const stopMinute = 26;
+    let maxVal = 0;
+    while (minute <= stopMinute) {
+      if (minute >10) {
+        newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 0);
+        
+        newPaths.forEach(newpath => {
+          let openvalves = newpath.OpenValves;
+          let openvalvesLength = openvalves.length
+          let flowNumber = newpath.TotalPressure(stopMinute + 1)
+          let differentPath = (newPaths.filter(np => np.OpenValves.length == totalValves - openvalvesLength && np.OpenValves.every(n=> !openvalves.includes(n)))).map(x=> x.TotalPressure(stopMinute+ 1)).sort()[0];
+          maxVal = differentPath + flowNumber > maxVal ? differentPath + flowNumber : maxVal
+        })
 
- // let hp = newPaths.filter(a => a.TotalFlowRate == highPath.TotalFlowRate)[0];
-  let totalPressure = highPath.TotalPressure(30);
-  return totalPressure;
+        //newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 2);
+        //newPaths.sort((a, b) => b.TotalPressure(26) - a.TotalPressure(26))
+        //newPaths.splice(1000);
+      }
+      
+      console.log(maxVal)
+
+      let oldPaths = newPaths.filter(np => np.Minute == minute)
+      minute++;
+
+      console.log(minute);
+      //logPath(oldPaths, minute);
+      newPaths = [];
+      oldPaths.forEach(oldPath => {
+        let openValves = oldPath.OpenValves ? oldPath.OpenValves : []; //oldPath.OpenValves;
+        let currentValve = scanOutput.find(v => v.Name == oldPath.ValveName);
+        let valves = scanOutput.filter(v => currentValve.TunnelToValve.includes(v.Name));
+        let currentFlow = oldPath.TotalFlowRate ?? 0;
+        valves.filter(v => v.Name != oldPath.PreviousValve).forEach(valve => {
+          let openValvesMinute = oldPath.OpenValvesMinute ? oldPath.OpenValvesMinute : null;
+          newPaths.push(new Step(valve.Name, minute, currentFlow, openValves, currentValve.Name, openValvesMinute))
+        })
+
+        if (currentValve.FlowRate > 0 && (!openValves || !openValves.find(o => o === currentValve.Name))) {
+          let openValvesMinute = oldPath.OpenValvesMinute ? [...oldPath.OpenValvesMinute] : null; //oldPath.OpenValvesMinute;
+          let newOpenValves = [...openValves]
+          // if (openValvesMinute?.length > 6) {
+          //   let b = 1;
+          // }
+
+          newOpenValves.push(currentValve.Name)
+          newOpenValves = newOpenValves.sort()
+          let minuteWithFlowrate = [minute, currentValve.FlowRate];
+
+          if (!openValvesMinute) {
+            openValvesMinute = [minuteWithFlowrate];
+          }
+          else {
+            openValvesMinute.push(minuteWithFlowrate);
+          }
+
+          let newFlow = currentFlow + currentValve.FlowRate
+          newPaths.push(new Step(currentValve.Name, minute, newFlow, newOpenValves, currentValve.Name, openValvesMinute))
+        }
+
+      });
+    }
+
+    // newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 0);
+    // let maxVal = 0 
+    // newPaths.forEach(newpath => {
+    //   let openvalves = newpath.OpenValves;
+    //   let flowNumber = newpath.TotalPressure(stopMinute + 1)
+    //   let differentPath = (newPaths.filter(np =>np.OpenValves.every(n=> !openvalves.includes(n)))).map(x=> x.TotalPressure(stopMinute+ 1)).sort()[0];
+    //   let t = 5;
+    //  maxVal = differentPath + flowNumber > maxVal ? differentPath + flowNumber : maxVal
+    // })
+    //let steps = newPaths.filter(np => np.Minute == minute)
+    let typ = maxVal;
+    let highPath = newPaths.sort((o1, o2) => o2.TotalPressure(stopMinute) - o1.TotalPressure(stopMinute))[0];
+
+    // let hp = newPaths.filter(a => a.TotalFlowRate == highPath.TotalFlowRate)[0];
+    let totalPressure = highPath.TotalPressure(30);
+    return totalPressure;
+  }
 }
 
 
@@ -122,7 +207,7 @@ function logPath(oldPaths: Step[], minute: number) {
   console.log(`minute:${minute} totalflow:${highPath.TotalFlowRate} openValves: ${highPath.OpenValves?.join(' ,')}`)
 }
 
-Promise.all([day16(1, true)]).then((answer) => console.log(answer.join(', ')))
+Promise.all([day16(2, true)]).then((answer) => console.log(answer.join(', ')))
 
 //answer1 2077
 //answer2 
--- out/day16.js
+++ out/day16.js
@@ -47,65 +47,131 @@ async function day16(part, print) {
         let tunnelstovalve = line.substring(line.indexOf('valves') + 6).split(',').map(valve => valve.trim());
         return new Valve(name, flowrate, tunnelstovalve);
     });
+    let totalValves = scanOutput.filter(so => so.FlowRate != undefined && so.FlowRate > 0).length;
     let minute = 0;
     let newPaths = [new Step("AA", 0)];
-    while (minute <= 30) {
-        if (minute == 24) {
-            newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 2);
-            newPaths.sort((a, b) => b.TotalPressure(30) - a.TotalPressure(30));
-            newPaths.splice(1000);
-            //let recurrentpaths= newPaths.filter(np => np.)
-        }
-        let oldPaths = newPaths.filter(np => np.Minute == minute);
-        minute++;
-        console.log(minute);
-        //logPath(oldPaths, minute);
-        newPaths = [];
-        oldPaths.forEach(oldPath => {
-            let openValves = oldPath.OpenValves ? oldPath.OpenValves : []; //oldPath.OpenValves;
-            let currentValve = scanOutput.find(v => v.Name == oldPath.ValveName);
-            let valves = scanOutput.filter(v => currentValve.TunnelToValve.includes(v.Name));
-            let currentFlow = oldPath.TotalFlowRate ?? 0;
-            valves.filter(v => v.Name != oldPath.PreviousValve).forEach(valve => {
-                let openValvesMinute = oldPath.OpenValvesMinute ? oldPath.OpenValvesMinute : null;
-                newPaths.push(new Step(valve.Name, minute, currentFlow, openValves, currentValve.Name, openValvesMinute));
-            });
-            if (currentValve.FlowRate > 0 && (!openValves || !openValves.find(o => o === currentValve.Name))) {
-                let openValvesMinute = oldPath.OpenValvesMinute ? [...oldPath.OpenValvesMinute] : null; //oldPath.OpenValvesMinute;
-                let newOpenValves = [...openValves];
-                // if (openValvesMinute?.length > 6) {
-                //   let b = 1;
-                // }
-                newOpenValves.push(currentValve.Name);
-                newOpenValves = newOpenValves.sort();
-                let minuteWithFlowrate = [minute, currentValve.FlowRate];
-                if (!openValvesMinute) {
-                    openValvesMinute = [minuteWithFlowrate];
-                }
-                else {
-                    openValvesMinute.push(minuteWithFlowrate);
-                }
-                let newFlow = currentFlow + currentValve.FlowRate;
-                newPaths.push(new Step(currentValve.Name, minute, newFlow, newOpenValves, currentValve.Name, openValvesMinute));
+    if (part == 1) {
+        while (minute <= 30) {
+            if (minute == 24) {
+                newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 2);
+                newPaths.sort((a, b) => b.TotalPressure(30) - a.TotalPressure(30));
+                newPaths.splice(1000);
             }
-        });
+            let oldPaths = newPaths.filter(np => np.Minute == minute);
+            minute++;
+            console.log(minute);
+            //logPath(oldPaths, minute);
+            newPaths = [];
+            oldPaths.forEach(oldPath => {
+                let openValves = oldPath.OpenValves ? oldPath.OpenValves : []; //oldPath.OpenValves;
+                let currentValve = scanOutput.find(v => v.Name == oldPath.ValveName);
+                let valves = scanOutput.filter(v => currentValve.TunnelToValve.includes(v.Name));
+                let currentFlow = oldPath.TotalFlowRate ?? 0;
+                valves.filter(v => v.Name != oldPath.PreviousValve).forEach(valve => {
+                    let openValvesMinute = oldPath.OpenValvesMinute ? oldPath.OpenValvesMinute : null;
+                    newPaths.push(new Step(valve.Name, minute, currentFlow, openValves, currentValve.Name, openValvesMinute));
+                });
+                if (currentValve.FlowRate > 0 && (!openValves || !openValves.find(o => o === currentValve.Name))) {
+                    let openValvesMinute = oldPath.OpenValvesMinute ? [...oldPath.OpenValvesMinute] : null; //oldPath.OpenValvesMinute;
+                    let newOpenValves = [...openValves];
+                    // if (openValvesMinute?.length > 6) {
+                    //   let b = 1;
+                    // }
+                    newOpenValves.push(currentValve.Name);
+                    newOpenValves = newOpenValves.sort();
+                    let minuteWithFlowrate = [minute, currentValve.FlowRate];
+                    if (!openValvesMinute) {
+                        openValvesMinute = [minuteWithFlowrate];
+                    }
+                    else {
+                        openValvesMinute.push(minuteWithFlowrate);
+                    }
+                    let newFlow = currentFlow + currentValve.FlowRate;
+                    newPaths.push(new Step(currentValve.Name, minute, newFlow, newOpenValves, currentValve.Name, openValvesMinute));
+                }
+            });
+        }
+        //let steps = newPaths.filter(np => np.Minute == minute)
+        let highPath = newPaths.sort((o1, o2) => o2.TotalPressure(30) - o1.TotalPressure(30))[0];
+        // let hp = newPaths.filter(a => a.TotalFlowRate == highPath.TotalFlowRate)[0];
+        let totalPressure = highPath.TotalPressure(30);
+        return totalPressure;
     }
-    //let steps = newPaths.filter(np => np.Minute == minute)
-    let highPath = newPaths.sort((o1, o2) => o2.TotalPressure(30) - o1.TotalPressure(30))[0];
-    // let hp = newPaths.filter(a => a.TotalFlowRate == highPath.TotalFlowRate)[0];
-    let totalPressure = highPath.TotalPressure(30);
-    // let answer = 0;
-    // allHighPaths.forEach(ahp => {
-    //   let totalPressure = ahp.TotalPressure(30);
-    //   answer = totalPressure > answer ? totalPressure : answer;
-    // })
-    //return answer;
+    else {
+        //part 2
+        const stopMinute = 26;
+        let maxVal = 0;
+        while (minute <= stopMinute) {
+            if (minute > 10) {
+                newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 0);
+                newPaths.forEach(newpath => {
+                    let openvalves = newpath.OpenValves;
+                    let openvalvesLength = openvalves.length;
+                    let flowNumber = newpath.TotalPressure(stopMinute + 1);
+                    let differentPath = (newPaths.filter(np => np.OpenValves.length == totalValves - openvalvesLength && np.OpenValves.every(n => !openvalves.includes(n)))).map(x => x.TotalPressure(stopMinute + 1)).sort()[0];
+                    maxVal = differentPath + flowNumber > maxVal ? differentPath + flowNumber : maxVal;
+                });
+                //newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 2);
+                //newPaths.sort((a, b) => b.TotalPressure(26) - a.TotalPressure(26))
+                //newPaths.splice(1000);
+            }
+            console.log(maxVal);
+            let oldPaths = newPaths.filter(np => np.Minute == minute);
+            minute++;
+            console.log(minute);
+            //logPath(oldPaths, minute);
+            newPaths = [];
+            oldPaths.forEach(oldPath => {
+                let openValves = oldPath.OpenValves ? oldPath.OpenValves : []; //oldPath.OpenValves;
+                let currentValve = scanOutput.find(v => v.Name == oldPath.ValveName);
+                let valves = scanOutput.filter(v => currentValve.TunnelToValve.includes(v.Name));
+                let currentFlow = oldPath.TotalFlowRate ?? 0;
+                valves.filter(v => v.Name != oldPath.PreviousValve).forEach(valve => {
+                    let openValvesMinute = oldPath.OpenValvesMinute ? oldPath.OpenValvesMinute : null;
+                    newPaths.push(new Step(valve.Name, minute, currentFlow, openValves, currentValve.Name, openValvesMinute));
+                });
+                if (currentValve.FlowRate > 0 && (!openValves || !openValves.find(o => o === currentValve.Name))) {
+                    let openValvesMinute = oldPath.OpenValvesMinute ? [...oldPath.OpenValvesMinute] : null; //oldPath.OpenValvesMinute;
+                    let newOpenValves = [...openValves];
+                    // if (openValvesMinute?.length > 6) {
+                    //   let b = 1;
+                    // }
+                    newOpenValves.push(currentValve.Name);
+                    newOpenValves = newOpenValves.sort();
+                    let minuteWithFlowrate = [minute, currentValve.FlowRate];
+                    if (!openValvesMinute) {
+                        openValvesMinute = [minuteWithFlowrate];
+                    }
+                    else {
+                        openValvesMinute.push(minuteWithFlowrate);
+                    }
+                    let newFlow = currentFlow + currentValve.FlowRate;
+                    newPaths.push(new Step(currentValve.Name, minute, newFlow, newOpenValves, currentValve.Name, openValvesMinute));
+                }
+            });
+        }
+        // newPaths = newPaths.filter(np => np.OpenValves != undefined && np.OpenValves.length > 0);
+        // let maxVal = 0 
+        // newPaths.forEach(newpath => {
+        //   let openvalves = newpath.OpenValves;
+        //   let flowNumber = newpath.TotalPressure(stopMinute + 1)
+        //   let differentPath = (newPaths.filter(np =>np.OpenValves.every(n=> !openvalves.includes(n)))).map(x=> x.TotalPressure(stopMinute+ 1)).sort()[0];
+        //   let t = 5;
+        //  maxVal = differentPath + flowNumber > maxVal ? differentPath + flowNumber : maxVal
+        // })
+        //let steps = newPaths.filter(np => np.Minute == minute)
+        let typ = maxVal;
+        let highPath = newPaths.sort((o1, o2) => o2.TotalPressure(stopMinute) - o1.TotalPressure(stopMinute))[0];
+        // let hp = newPaths.filter(a => a.TotalFlowRate == highPath.TotalFlowRate)[0];
+        let totalPressure = highPath.TotalPressure(30);
+        return totalPressure;
+    }
 }
 function logPath(oldPaths, minute) {
     let highPath = oldPaths.sort((o1, o2) => o2.TotalFlowRate - o1.TotalFlowRate)[0];
     console.log(`minute:${minute} totalflow:${highPath.TotalFlowRate} openValves: ${highPath.OpenValves?.join(' ,')}`);
 }
-Promise.all([day16(1, true)]).then((answer) => console.log(answer.join(', ')));
-//answer1 2054
+Promise.all([day16(2, true)]).then((answer) => console.log(answer.join(', ')));
+//answer1 2077
 //answer2 
 //# sourceMappingURL=day16.js.map
